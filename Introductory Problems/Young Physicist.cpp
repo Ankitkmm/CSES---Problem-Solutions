5c707521041109cbd9b05f1212e42f4cd060a321
Ankit Kumar <91867197+Ankitkmm@users.noreply.github.com>
2022-12-06 08:34:03 +0530

    Add files via upload

---
+++ Introductory Problems/Young Physicist.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+ll n;
+cin >> n;
+if(n > 2){
+ if(n % 2 == 0 ){
+        cout<<"YES";
+ }else{
+     cout<<"NO";
+ }
+}
+ else{
+ cout << "NO";
+}
+}
---
+++ Introductory Problems/contest.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+string s,k;
+
+getline(cin, s);
+for(int i=0; i<s.length(); i++){
+    k = (char) tolower(s[i]);
+}
+cout <<  k << endl;
+}
